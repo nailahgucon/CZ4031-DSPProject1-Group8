4cc83235a14b05b4b80e54d498963a8ed4880dc1
nailahgucon <nailahgucon@gmail.com>
2023-03-02 18:22:40 +0800

    added comprehensive comments for BPlusTree implementation

--- BPlusTree/LeafNode.java
+++ BPlusTree/LeafNode.java
@@ -23,7 +23,18 @@ public class LeafNode extends Node{
         return addresses.get(i);
     }
 
-    // Sets the address of the current node
+    /**
+    SetAddress(int key, Address address): sets an address for a given key in a leaf node (current node).
+
+    If the node is empty:
+        sets the key and address at index 0
+    Else, it uses the setKey method from the Node class to set the key at the appropriate index and
+    adds the address to the addresses list at the same index.
+    The method then performs a for loop to shift all the subsequent addresses to the right to make room for
+    the new address at the given index.
+    Finally, it sets the index and address in the addresses list of the current node.
+    Returns the index at which the key was inserted.
+     */
     public int setAddress(int key, Address address){
         if (this.getAddresses().size() == 0) {
             this.setKey(key);
@@ -36,7 +47,6 @@ public class LeafNode extends Node{
         for (int i = addresses.size() -2; i >= index; i--)
             addresses.set(i+1, addresses.get(i));
 
-        //set the key index in the current node and the address of the current node
         addresses.set(index, address);
         return index;
     }
@@ -59,10 +69,11 @@ public class LeafNode extends Node{
         this.nextNode = nextNode;
     }
 
+    /**
+    doSeparation(): Deletes all keys and resets all the addresses
+    */
     public void doSeparation(){
-        //Delete all keys
         doKeysDeletion();
-        //reset all the addresses
         addresses = new ArrayList<Address>();
     }
 
